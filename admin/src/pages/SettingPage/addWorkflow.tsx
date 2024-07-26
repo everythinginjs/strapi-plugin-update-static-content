061fbebbeb2f32b283bacaedd65b5c0357421bb5
Mathias ROSA <mat.ros@outlook.fr>
2024-07-26 11:11:32 +1000

    clearer navigation

--- admin/src/pages/SettingPage/addWorkflow.tsx
+++ admin/src/pages/SettingPage/addWorkflow.tsx
@@ -5,6 +5,7 @@ import useFormattedLabel from '../../hooks/useFormattedLabel';
 import { useFetchClient } from '@strapi/helper-plugin';
 import pluginId from '../../pluginId';
 import { useHistory } from 'react-router-dom';
+import { ArrowLeft } from '@strapi/icons';
 
 export default function AddNewWorkflow() {
   const { goBack } = useHistory();
@@ -15,7 +16,6 @@ export default function AddNewWorkflow() {
   const [githubToken, setGithubToken] = useState('');
 
   const { post } = useFetchClient();
-
 
   const PAGE_TITLE = 'Add New Workflow';
   const HEADER_TITLE = 'Add New Workflow';
@@ -39,102 +39,114 @@ export default function AddNewWorkflow() {
   const PLACEHOLDER_WORKFLOWID = useFormattedLabel('settings.fields.placeholder.workflowid');
   const PLACEHOLDER_BRANCH = useFormattedLabel('settings.fields.placeholder.branch');
   const BUTTON_DETAILS = useFormattedLabel('button.details');
-  
+
+  const BACK_BUTTON = useFormattedLabel('button.back');
+  const SAVE_BUTTON = useFormattedLabel('button.save')
+
   return (
     <PageWrapper
-      baseHeaderLayout={<BaseHeaderLayout title={HEADER_TITLE} subtitle={HEADER_SUBTITLE} />}
-      pageTitle={PAGE_TITLE}
-    >
-      <form onSubmit={async (e) => {
-        e.preventDefault();
-        if (githubToken && githubAccount && repo && workflow && branch) {
-          await post(`/${pluginId}/config`, {
-            githubToken,
-            githubAccount,
-            repo,
-            workflow,
-            branch,
-          });
-          goBack();
-        }
-        else {
-          console.error('Please fill all the fields');
-        }
-      }}>
-      <Flex direction="column" gap={3} alignItems="normal">
-        <TextInput
-          type="text"
-          label={GITHUB_TOKEN}
-          aria-label={GITHUB_TOKEN}
-          name="githubToken"
-          value={githubToken}
-          onChange={(e) => setGithubToken(e.target.value)}
-          placeholder={PLACEHOLDER_GITHUB_TOKEN}
-          required
-          HintMessage={
-            <Typography variant="omega">
-              {HINT_GITHUB_TOKEN}{' '}
-              <Link
-                href="https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token"
-                isExternal
-              >
-                {BUTTON_DETAILS}
-              </Link>
-            </Typography>
+      baseHeaderLayout={
+        <BaseHeaderLayout
+          title={HEADER_TITLE}
+          subtitle={HEADER_SUBTITLE}
+          navigationAction={
+            <Link to={`/settings/${pluginId}`} startIcon={<ArrowLeft />}>
+              {BACK_BUTTON}
+            </Link>
           }
         />
-        <TextInput
-          type="text"
-          label={OWNER}
-          aria-label={OWNER}
-          name="githubAccount"
-          value={githubAccount}
-          onChange={(e) => setGithubAccount(e.target.value)}
-          placeholder={PLACEHOLDER_OWNER}
-          required
-          HintMessage={<Typography variant="omega">{HINT_OWNER}</Typography>}
-        />
-        <TextInput
-          type="text"
-          label={REPO}
-          aria-label={REPO}
-          name="repo"
-          value={repo}
-          onChange={(e) => setRepo(e.target.value)}
-          placeholder={PLACEHOLDER_REPO}
-          required
-          HintMessage={<Typography variant="omega">{HINT_REPO}</Typography>}
-        />
-        <TextInput
-          type="text"
-          label={BRANCH}
-          aria-label={BRANCH}
-          name="branch"
-          value={branch}
-          onChange={(e) => setBranch(e.target.value)}
-          placeholder={PLACEHOLDER_BRANCH}
-          required
-          HintMessage={<Typography variant="omega">{HINT_BRANCH}</Typography>}
-        />
-        <TextInput
-          type="text"
-          label={WORKFLOWID}
-          aria-label={WORKFLOWID}
-          name="workflow_id"
-          value={workflow}
-          onChange={(e) => setWorkflow(e.target.value)}
-          placeholder={PLACEHOLDER_WORKFLOWID}
-          required
-          HintMessage={<Typography variant="omega">{HINT_WORKFLOWID}</Typography>}
-        />
-
-
-      </Flex>
+      }
+      pageTitle={PAGE_TITLE}
+    >
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          if (githubToken && githubAccount && repo && workflow && branch) {
+            await post(`/${pluginId}/config`, {
+              githubToken,
+              githubAccount,
+              repo,
+              workflow,
+              branch,
+            });
+            goBack();
+          } else {
+            console.error('Please fill all the fields');
+          }
+        }}
+      >
+        <Flex direction="column" gap={3} alignItems="normal">
+          <TextInput
+            type="text"
+            label={GITHUB_TOKEN}
+            aria-label={GITHUB_TOKEN}
+            name="githubToken"
+            value={githubToken}
+            onChange={(e) => setGithubToken(e.target.value)}
+            placeholder={PLACEHOLDER_GITHUB_TOKEN}
+            required
+            autoComplete="hidden"
+            HintMessage={
+              <Typography variant="omega">
+                {HINT_GITHUB_TOKEN}{' '}
+                <Link
+                  href="https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token"
+                  isExternal
+                >
+                  {BUTTON_DETAILS}
+                </Link>
+              </Typography>
+            }
+          />
+          <TextInput
+            type="text"
+            label={OWNER}
+            aria-label={OWNER}
+            name="githubAccount"
+            value={githubAccount}
+            onChange={(e) => setGithubAccount(e.target.value)}
+            placeholder={PLACEHOLDER_OWNER}
+            required
+            HintMessage={<Typography variant="omega">{HINT_OWNER}</Typography>}
+          />
+          <TextInput
+            type="text"
+            label={REPO}
+            aria-label={REPO}
+            name="repo"
+            value={repo}
+            onChange={(e) => setRepo(e.target.value)}
+            placeholder={PLACEHOLDER_REPO}
+            required
+            HintMessage={<Typography variant="omega">{HINT_REPO}</Typography>}
+          />
+          <TextInput
+            type="text"
+            label={BRANCH}
+            aria-label={BRANCH}
+            name="branch"
+            value={branch}
+            onChange={(e) => setBranch(e.target.value)}
+            placeholder={PLACEHOLDER_BRANCH}
+            required
+            HintMessage={<Typography variant="omega">{HINT_BRANCH}</Typography>}
+          />
+          <TextInput
+            type="text"
+            label={WORKFLOWID}
+            aria-label={WORKFLOWID}
+            name="workflow_id"
+            value={workflow}
+            onChange={(e) => setWorkflow(e.target.value)}
+            placeholder={PLACEHOLDER_WORKFLOWID}
+            required
+            HintMessage={<Typography variant="omega">{HINT_WORKFLOWID}</Typography>}
+          />
+        </Flex>
 
         <Button size="M" marginTop={4} type="submit">
-          Save
+          {SAVE_BUTTON}
         </Button>
-
       </form>
     </PageWrapper>
   );
--- admin/src/pages/SettingPage/index.tsx
+++ admin/src/pages/SettingPage/index.tsx
@@ -1,11 +1,8 @@
-import { BaseHeaderLayout, Link, Stack, Typography } from '@strapi/design-system';
-import { CheckPagePermissions, useFetchClient } from '@strapi/helper-plugin';
+import { BaseHeaderLayout } from '@strapi/design-system';
+import { CheckPagePermissions, } from '@strapi/helper-plugin';
 import PageWrapper from '../../components/PageWrapper';
-import useFetch from '../../hooks/useFetch';
 import useFormattedLabel from '../../hooks/useFormattedLabel';
 import pluginPermissions from '../../permissions';
-import pluginId from '../../pluginId';
-import Config from '../../../../types/Config';
 import ConfigsTable from '../../components/ConfigsTable';
 
 export default function ProtectedPage() {
@@ -17,23 +14,14 @@ export default function ProtectedPage() {
 }
 
 const SettingPage = () => {
-  // Hooks
-  const [data, isLoading] = useFetch<Config>(`/${pluginId}/config`);
-  const { branch, githubToken, githubAccount, repo, workflow } = data;
-  const { post } = useFetchClient();
-
   // Translations
   const PAGE_TITLE = useFormattedLabel('settings.pagetitle');
   const HEADER_TITLE = useFormattedLabel('settings.headers.title');
   const HEADER_SUBTITLE = useFormattedLabel('settings.headers.subtitle');
 
-  const setConfig = (data: Config) => {
-    post(`/${pluginId}/config`, data);
-  };
 
   return (
     <PageWrapper
-      isLoading={isLoading}
       baseHeaderLayout={<BaseHeaderLayout title={HEADER_TITLE} subtitle={HEADER_SUBTITLE} />}
       pageTitle={PAGE_TITLE}
     >
